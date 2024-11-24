1579eee07539542d0c7123c6e6f5fe3f1ba4034d
Owen Pratama S. <92712038+2122600018-Owen@users.noreply.github.com>
2024-11-24 14:39:32 +0700

    Update konek.php

--- konek.php
+++ konek.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "as_db");
+$con = mysqli_connect("as-db.mysql.database.azure.com", "zwfvqusiix", "xKnW$jS1dx8SKF8S", "as-db");
 ?>
 
 <h1 style="font-size: 100px">70</h1>
