818dbbfdc30bc26a5c3a096608c7c1c097c79c02
Owen Pratama S. <92712038+2122600018-Owen@users.noreply.github.com>
2024-11-24 13:37:05 +0700

    Update fetch_data.php

--- fetch_data.php
+++ fetch_data.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "as_db");
+$con = mysqli_connect("localhost", "root", "", "aquasmart-database");
 
 $dataHistory = mysqli_query($con, "SELECT * FROM tandon ORDER BY id DESC LIMIT 10");
 $timeLabels = [];
